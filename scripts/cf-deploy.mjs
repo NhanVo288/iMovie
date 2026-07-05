5305ded7df38bac31adb840e921c9bc6073ff661
vnhan28 <vnhan2808@gmail.com>
2026-07-05 22:09:15 +0700

    update

--- scripts/cf-deploy.mjs
+++ scripts/cf-deploy.mjs
@@ -18,8 +18,8 @@ import { resolve } from 'node:path'
 
 const CACHE_DIR = resolve('.open-next/cache')
 const CACHE_BAK = resolve('.open-next/cache.skip-populate')
-const SITE_HOST = 'www.iMovie.space'
-const ZONE_NAME = 'iMovie.space'
+const SITE_HOST = 'www.imovie.space'
+const ZONE_NAME = 'imovie.space'
 const INDEXNOW_KEY = 'fd71a860ed122d006df9ba7c2c529b88'
 
 function run(args) {
